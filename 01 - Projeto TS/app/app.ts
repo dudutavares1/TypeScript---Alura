aeb1a84cac1e6aecadc6a1579104a42867ccc782
Dudu Tavares <skynetdudu@icloud.com>
2021-08-11 01:37:14 -0300

    aula 4
    impostando dom

--- 01 - Projeto TS/app/app.ts
+++ 01 - Projeto TS/app/app.ts
@@ -1,1 +1,7 @@
-const controller = new NegociacaoController
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+
+const controller = new NegociacaoController();
+const form = document.querySelector(".form");;
+form.addEventListener("submit", event => {
+  controller.adiciona();
+});
