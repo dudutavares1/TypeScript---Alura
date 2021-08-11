97c2748357c6054b8a9e731490f85ccccc4bfe66
Dudu Tavares <skynetdudu@icloud.com>
2021-08-11 00:43:23 -0300

    aula 3
    aula 3

--- 01 - Projeto TS/app/app.ts
+++ 01 - Projeto TS/app/app.ts
@@ -1,4 +1,1 @@
-import { Negociacao } from './models/negociacao.js';
-
-const negociacao = new Negociacao(new Date());
-console.log(negociacao.volume);
+const controller = new NegociacaoController
---
+++ 01 - Projeto TS/dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,12 @@
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#Quantidade');
+        this.inputValor = document.querySelector('#Valor');
+    }
+    adiciona() {
+        console.log(this.inputData);
+        console.log(this.inputQuantidade);
+        console.log(this.inputValor);
+    }
+}
